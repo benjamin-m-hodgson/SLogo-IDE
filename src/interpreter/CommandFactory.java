e4f781034b6e1020ce6013eb8dad59e034c895c4
Sarah Bland <scb49@duke.edu>
2018-02-23 15:47:10 -0500

    Command tree reader error checking

--- src/interpreter/CommandFactory.java
+++ src/interpreter/CommandFactory.java
@@ -1,11 +1,18 @@
 package interpreter;
+import java.util.List;
 
 public class CommandFactory {
 	public CommandFactory() {
-	
+		
 	}
 
-	public Command makeCommand() {
-		
+	public Command makeCommand(boolean isDouble, String commandName, List<Command> commandArgs) {
+		if(isDouble) {
+			double doubleArg = Double.parseDouble(commandName);
+			return new DoubleCommand(doubleArg);
+		}
+		else {
+			
+		}
 	}
 }
--- src/interpreter/CommandTreeReader.java
+++ src/interpreter/CommandTreeReader.java
@@ -1,5 +1,34 @@
 package interpreter;
 
 public class CommandTreeReader {
-	
+	CommandFactory myCommandFactory;
+	public CommandTreeReader(){
+		myCommandFactory = new CommandFactory();
+	}
+	protected boolean treeIsComplete(CommandNode root) {
+		if(root.getIsDouble()) {
+			return true;
+		}
+		int completedChildren = 0;
+		for (CommandNode n: root.getChildren()) {
+			if(treeIsComplete(n)) {
+				completedChildren++;
+			}
+		}
+		return completedChildren==root.getNumArgs();
+	}
+	private int checkDoubleChildren(CommandNode root) {
+		
+	}
+	protected Command compressTree(CommandNode root) {
+		int numArgs = root.getNumArgs();
+		for(CommandNode k: root.getChildren()) {
+			if(root.isDouble) {
+				return myCommandFactory.makeDoubleCommand(root.getInfo());
+			}
+			else {
+				
+			}
+		}
+	}
 }
