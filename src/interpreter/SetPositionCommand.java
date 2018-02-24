fe2fd881ae4e5636f1e602460f3f6b0f8eff5501
Sarah Bland <scb49@duke.edu>
2018-02-24 16:25:23 -0500

    SetPositionCommand

--- src/interpreter/SetPositionCommand.java
+++ src/interpreter/SetPositionCommand.java
@@ -1,7 +1,17 @@
 package interpreter;
 
-public class SetPositionCommand {
-	private double myNewX;
-	private double myNewY;
-	
+public class SetPositionCommand implements Command{
+	private Command myNewXCommand;
+	private Command myNewYCommand;
+	private Turtle myTurtle;
+	protected SetPositionCommand(Command newXCommand, Command newYCommand, Turtle turtle) {
+		myNewXCommand = newXCommand;
+		myNewYCommand = newYCommand;
+		myTurtle = turtle;
+	}
+	public double execute() {
+		double newX = myNewXCommand.execute();
+		double newY = myNewYCommand.execute();
+		return myTurtle.setXY(newX, newY);
+	}
 }
