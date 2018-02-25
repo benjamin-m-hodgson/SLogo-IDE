d71068fd57db78ae1ff42fee03aa91f5f73fec57
Ben Hodgson <bmh43@duke.edu>
2018-02-25 17:16:10 -0500

    Made the console area un-editable by the user

--- src/screen/panel/TextPanel.java
+++ src/screen/panel/TextPanel.java
@@ -54,9 +54,11 @@ public class TextPanel implements Panel {
     }
     
     private TextArea makeConsoleArea() {
-	TextArea inputArea = new TextArea();
-	inputArea.setId("consoleField");
-	return inputArea;
+	TextArea consoleArea = new TextArea();
+	consoleArea.setId("consoleField");
+	consoleArea.setPromptText(PROGRAM_CONTROLLER.resourceDisplayText("ConsolePrompt"));
+	consoleArea.setEditable(false);
+	return consoleArea;
     }
 
 }
