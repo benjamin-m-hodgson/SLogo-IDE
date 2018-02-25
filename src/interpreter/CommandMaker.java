44819b002aef7b5dbca064b908307ad8638cba5e
Susie Choi <sc477@duke.edu>
2018-02-25 09:34:10 -0500

    debugged passing of turtle+arrays from parser-->commandmaker

--- src/interpreter/CommandMaker.java
+++ src/interpreter/CommandMaker.java
@@ -14,37 +14,43 @@ package interpreter;
 //}
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-public class CommandMaker {
-	
+class CommandMaker {
+
 	public static final String DEFAULT_FILEPATH = "interpreter/";
 	public static final String DEFAULT_LANGUAGE = "English";
 	public static final String DEFAULT_NUM_ARGS_FILE = "NumArgsForCommands";
 	public static final String DEFAULT_COMMAND_IDENTIFIER = "Command"; //TODO allow this to be client-specified
-	
+	public static final String[] DEFAULT_CONTROLFLOW_IDENTIFIERS = {"Repeat", "DoTimes", "For"};
+
 	private ArrayList<Turtle> myTurtles; 
 	private String myLanguageFileName; 
 	private CommandTreeBuilder myCommandTreeBuilder; 
 	private HashMap<String, Double> myVariables; 
-	
-	public CommandMaker() {
+	private ArrayList<String> myListForBuilder; 
+
+	protected CommandMaker() {
 		this(DEFAULT_FILEPATH+DEFAULT_LANGUAGE, DEFAULT_FILEPATH+DEFAULT_NUM_ARGS_FILE);
 	}
-	
-	public CommandMaker(String languageFileName, String numArgsFileName) {
+
+	protected CommandMaker(String languageFileName, String numArgsFileName) {
 		myLanguageFileName = languageFileName;
 		myCommandTreeBuilder = new CommandTreeBuilder(numArgsFileName); 
 		myVariables = new HashMap<String, Double>(); 
+		myListForBuilder = new ArrayList<String>();
 	}
-	
-	public double parseValidTextArray(String turtleName, String[] userInput, String[] typesOfInput) throws TurtleNotFoundException {
+
+	protected double parseValidTextArray(String turtleName, String[] userInput, String[] typesOfInput) throws TurtleNotFoundException {
+		myListForBuilder = new ArrayList<String>(); 
 		return parseValidTextArray(turtleName, userInput, typesOfInput, DEFAULT_COMMAND_IDENTIFIER);
 	}
-	
-	public double parseValidTextArray(String turtleName, String[] userInput, String[] typesOfInput, String commandIdentifier) throws TurtleNotFoundException {
+
+	private double parseValidTextArray(String turtleName, String[] userInput, String[] typesOfInput, String commandIdentifier) throws TurtleNotFoundException {
+		//		for (String s : userInput) System.out.println(s);
 		String[] commandTypes = new String[userInput.length];
 		for (int idx = 0; idx < userInput.length; idx++) {
 			if (typesOfInput[idx].equals(commandIdentifier)) {
@@ -52,31 +58,40 @@ public class CommandMaker {
 			}
 		}
 		Turtle identifiedTurtle = null; 
-		boolean foundTurtle = false; 
-		for (Turtle turtle : myTurtles) {
-			if (turtle.getName().equals(turtleName)) {
-				identifiedTurtle = turtle; 
-				foundTurtle = true; 
-			}
-		}
-		if (! foundTurtle) {
-			throw new TurtleNotFoundException(turtleName);
-		}
+		//		boolean foundTurtle = false; 
+		//		for (Turtle turtle : myTurtles) {
+		//			if (turtle.getName().equals(turtleName)) {
+		//				identifiedTurtle = turtle; 
+		//				foundTurtle = true; 
+		//			}
+		//		}
+		//		if (! foundTurtle) {
+		//			throw new TurtleNotFoundException(turtleName);
+		//		}
+		makeListForBuilder(myListForBuilder, userInput, commandTypes, 1, DEFAULT_CONTROLFLOW_IDENTIFIERS);
 		return myCommandTreeBuilder.buildAndExecute(identifiedTurtle, userInput, commandTypes, typesOfInput); 
 	}
-	
+
+	private void makeListForBuilder(ArrayList<String> currCommandList, String[] inputArray, String[] commandTypes, int numTimesToAdd, String[] controlFlowIdentifiers) {
+		for (int i = 0; i < inputArray.length; i++) {
+			if (Arrays.asList(controlFlowIdentifiers).contains(commandTypes[i])) {
+				
+			}
+		}
+	}
+
 	private String getCommandType(String text) {
-        RegexMatcher regexMatcher = new RegexMatcher(myLanguageFileName);
-        String commandType = regexMatcher.findMatchingKey(text);
-        return commandType;
-    }
-	
+		RegexMatcher regexMatcher = new RegexMatcher(myLanguageFileName);
+		String commandType = regexMatcher.findMatchingKey(text);
+		return commandType;
+	}
+
 	protected void changeLanguageFile(String fileName) {
 		myLanguageFileName = fileName; 
 	}
-	
+
 	protected Map<String, Double> getVariables() {
 		return Collections.unmodifiableMap(myVariables);
 	}
-	
+
 }
--- src/interpreter/TextFieldParser.java
+++ src/interpreter/TextFieldParser.java
@@ -30,7 +30,7 @@ import java.util.Map;
 import java.util.Queue;
 
 class TextFieldParser {	
-	
+
 	public static final String DEFAULT_FILEPATH = "interpreter/";
 	public static final String DEFAULT_SYNTAX_FILENAME = "Syntax";
 	public static final String DEFAULT_LANGUAGE = "English";
@@ -40,26 +40,26 @@ class TextFieldParser {
 	private String mySyntaxFileName; 
 	private CommandMaker myCommandMaker; 
 	private Queue<Command> myCommandQueue;
-	
+
 	protected TextFieldParser() {
 		this(DEFAULT_FILEPATH+DEFAULT_SYNTAX_FILENAME, DEFAULT_LANGUAGE, DEFAULT_FILEPATH+DEFAULT_NUM_ARGS_FILE);
 	}
-	
+
 	protected TextFieldParser(String languageFileName) {
 		this(DEFAULT_FILEPATH+DEFAULT_SYNTAX_FILENAME, languageFileName, DEFAULT_FILEPATH+DEFAULT_NUM_ARGS_FILE);
 	}
-	
+
 	protected TextFieldParser(String syntaxFileName, String languageFileName, String numArgsFileName) {
 		mySyntaxFileName = syntaxFileName;
 		myCommandMaker = new CommandMaker(languageFileName, numArgsFileName); 
 		myCommandQueue = new LinkedList<Command>(); 
 	}
-	
-	
-	
-	
 
-	
+
+
+
+
+
 	/**
 	 * Returns a Queue of commands given a String of concatenated commands (chops up the commands 
 	 * and sends them individually to CommandMaker)
@@ -68,19 +68,19 @@ class TextFieldParser {
 	protected double parseText(String userInputString) throws TurtleNotFoundException {
 		String[] userInputByLine = userInputString.split("\\r?\\n");
 		String[] userInputTypes = new String[userInputByLine.length];
-		
+
 		RegexMatcher regexMatcher = new RegexMatcher(mySyntaxFileName);
 		for (int idx = 0; idx < userInputByLine.length; idx++) {
 			userInputTypes[idx] = regexMatcher.findMatchingKey(userInputByLine[idx].substring(0, 1));
 		}
-		
+
 		ArrayList<String> nonCommentInputByLine = new ArrayList<String>();
 		for (int idx = 0; idx < userInputTypes.length; idx ++) {
 			if (! userInputTypes[idx].equals(DEFAULT_COMMENT_SYMBOL)) {
 				nonCommentInputByLine.add(userInputByLine[idx]);
 			}
 		}
-		
+
 		ArrayList<String> tokenizedInput = new ArrayList<String>();
 		for (int idx = 0; idx < nonCommentInputByLine.size(); idx ++) {
 			String[] whiteSpaceSplitLine = nonCommentInputByLine.get(idx).split("\\s+");
@@ -88,62 +88,63 @@ class TextFieldParser {
 				tokenizedInput.add(token);
 			}
 		}
-		
+
 		String[] tokenizedInputArray = tokenizedInput.toArray(new String[tokenizedInput.size()]);
 //		for (String s : tokenizedInputArray) {
 //			System.out.println(s);
 //		}
 		return parseTextArray(tokenizedInputArray);
 	}
-	
+
 	private double parseTextArray(String[] userInputArray) throws TurtleNotFoundException {
 		String[] listOfTypes = new String[userInputArray.length];
 		RegexMatcher regexMatcher = new RegexMatcher(mySyntaxFileName);
 		for (int idx = 0; idx < userInputArray.length; idx++) {
 			listOfTypes[idx] = regexMatcher.findMatchingKey(userInputArray[idx]);
+//			System.out.println(listOfTypes[idx]+" "+userInputArray[idx]);
 		}
-		double finalReturnVal = myCommandMaker.parseValidTextArray(userInputArray[0], Arrays.copyOfRange(userInputArray, 1, userInputArray.length-1), listOfTypes); 
+		double finalReturnVal = myCommandMaker.parseValidTextArray(userInputArray[0], Arrays.copyOfRange(userInputArray, 1, userInputArray.length), Arrays.copyOfRange(listOfTypes, 1, listOfTypes.length)); 
 		return finalReturnVal; 
 	}
-    
-    
-    
-    
-    // GETTERS
+
+
+
+
+	// GETTERS
 	/**
 	 * Returns and UnmodifiableMap of string variable keys to their double values
 	 */
 	protected Map<String, Double> getVariables() {
 		return myCommandMaker.getVariables();
 	}
-	
+
 	/**
 	 * Returns an ImmutableQueue of the Command Queue (where commands are Command objects) 
 	 */
 	protected Queue<Command> getCurrentCommandQueue() {
 		return myCommandQueue;
 	}
-	
-	
-	
-	
+
+
+
+
 	// SETTERS
 	protected void changeLanguageFile(String fileName) {
 		myCommandMaker.changeLanguageFile(fileName);
 	}
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 	public static void main(String[] args) {
 		TextFieldParser testingParser = new TextFieldParser();
 		try {
-			testingParser.parseText("#hello\nfd 50 fd 50\n#bk 50");
+			testingParser.parseText("turtle\n#hello\nfd 50 fd 20\n#bk 50");
 		} catch (Exception e) {
-			
+
 		}
 	}
-	
+
 }
