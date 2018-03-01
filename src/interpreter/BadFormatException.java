9796256fae8c102e28f6df3ac14423b066576c29
Ben Hodgson <bmh43@duke.edu>
2018-02-28 21:54:57 -0500

    Made bad format exception information more detailed

--- src/interpreter/BadFormatException.java
+++ src/interpreter/BadFormatException.java
@@ -2,6 +2,6 @@ package interpreter;
 
 public class BadFormatException extends Exception {
 	public BadFormatException(String text) {
-		super("BadFormatException "+text);
+		super("BadFormatException: Unsupported syntax '" + text + "'");
 	}
 }
